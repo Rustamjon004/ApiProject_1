8b61a3e0bc78310a4b8451b17fb5ba1f9e99f4f2
rustam <odiloffr@gmail.com>
2025-02-01 20:23:25 +0500

    Updated admin page

--- students/admin.py
+++ students/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
 
 from rest_framework.authtoken.models import Token
 
-admin.site.register(Token)
+from students.models import Student, Course
 
+admin.site.register(Token)
+admin.site.register(Student)
+
+admin.site.register(Course)
 
 # Register your models here.
